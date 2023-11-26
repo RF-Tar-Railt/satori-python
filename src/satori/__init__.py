b91549db6b1b97491f20d77331b7262aa5979fd8
RF-Tar-Railt <3165388245@qq.com>
2023-11-27 01:38:32 +0800

    :bookmark: version 0.9.0

--- src/satori/__init__.py
+++ src/satori/__init__.py
@@ -1,6 +1,7 @@
 from .config import WebhookInfo as WebhookInfo
 from .config import WebsocketsInfo as WebsocketsInfo
 from .const import Api as Api
+from .const import EventType as EventType
 from .element import At as At
 from .element import Audio as Audio
 from .element import Author as Author
