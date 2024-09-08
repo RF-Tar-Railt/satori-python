8c956520677dba195821383d8df7d47f373e8e8a
RF-Tar-Railt <rf_tar_railt@qq.com>
2024-09-09 02:24:32 +0800

    :art: format

--- src/satori/utils.py
+++ src/satori/utils.py
@@ -4,14 +4,14 @@ import socket
 def get_public_ip():
     st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        st.connect(('10.255.255.255', 1))
+        st.connect(("10.255.255.255", 1))
         IP = st.getsockname()[0]
     except Exception:
-        IP = 'localhost'
+        IP = "localhost"
     finally:
         st.close()
     return IP
 
 
-if __name__ == '__main__':
-    print(get_public_ip())
+if __name__ == "__main__":
+    print(get_public_ip())  # noqa: T201
