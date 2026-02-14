93d3aab7a461c0749c035287d58c169ee3f98d3b
RF-Tar-Railt <rf_tar_railt@qq.com>
2026-02-14 10:11:26 +0800

    :bookmark: version 0.18.1 & adapter onebot 0.3.1

--- src/satori/__init__.py
+++ src/satori/__init__.py
@@ -42,7 +42,7 @@ from .model import Role as Role
 from .model import Upload as Upload
 from .model import User as User
 
-__version__ = "0.18.0"
+__version__ = "0.18.1"
 
 
 MessageReceipt = MessageObject
